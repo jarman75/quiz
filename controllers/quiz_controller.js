ed9ffbc0dab8ae2a913a6a6e3dddae975440cf8a
Armando <jarman75@hotmail.es>
2015-08-02 14:10:48 +0200

    Lista de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,5 +1,36 @@
 var models = require('../models/models.js');
-//Get /quizes/question
+
+
+//Get /quizes/:id
+exports.show = function(req, res) {
+  models.Quiz.find(req.params.quizId).then(function(quiz) {
+    res.render('quizes/show', {quiz: quiz});
+  })
+};
+
+//Get /quizes/:id/answer
+exports.answer = function (req, res){
+  models.Quiz.find(req.params.quizId).then(function(quiz){
+    if (req.query.respuesta === quiz.respuesta) {
+      res.render('quizes/answer', {quiz:quiz, respuesta: 'Correcto'});
+    }else{
+      res.render('quizes/answer', {quiz:quiz, respuesta: 'Incorrecto'});
+    }
+  })
+};
+
+//Get /quizes
+exports.index = function(req, res) {
+  models.Quiz.findAll().then(function(quizes){
+    res.render('quizes/index.ejs', {quizes: quizes});
+  })
+};
+
+exports.author = function (req, res) {
+  res.render('author');
+}
+
+/*//Get /quizes/question
 exports.question = function(req, res) {
   models.Quiz.findAll().success(function(quiz){
       res.render('quizes/question', {pregunta: quiz[0].pregunta});
@@ -15,8 +46,4 @@ exports.answer = function (req, res) {
       res.render('quizes/answer', {respuesta: 'Incorrecto'});
     }
   })
-};
-
-exports.author = function (req, res) {
-  res.render('author');
-}
+}; */
